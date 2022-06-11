64f60fcc2ac26c5fa3cb19a3304a79aca6160c8b
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2022-06-10 21:25:06 -0700

    hasura work

--- functions/catClick.js
+++ functions/catClick.js
@@ -1,33 +1,43 @@
 const axios = require("axios");
 
 exports.handler = async (event, context) => {
-  console.log("clicked for cat");
-  if (event.httpMethod !== "GET") {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({
-        message: `wrong method. You tried ${event.httpMethod}`,
-      }),
-    };
-  }
-  let img;
-
   try {
-    const res = await axios({
-      method: "GET",
-      url: "https://api.thecatapi.com/v1/images/search",
-    });
-    img = res.data[0].url;
-    console.log("this is img", img);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `${img}`,
-      }),
-    };
+    console.log("clicked for cat");
+    if (event.httpMethod !== "GET") {
+      return {
+        statusCode: 405,
+        body: JSON.stringify({
+          message: `wrong method. You tried ${event.httpMethod}`,
+        }),
+      };
+    }
+
+    let img;
+
+    try {
+      const res = await axios({
+        method: "GET",
+        url: "https://api.thecatapi.com/v1/images/search",
+      });
+      img = res.data[0].url;
+      console.log("this is img", img);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `${img}`,
+        }),
+      };
+    } catch (err) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `${err.message}`,
+        }),
+      };
+    }
   } catch (err) {
     return {
-      statusCode: 400,
+      statusCode: 500,
       body: JSON.stringify({
         message: `${err.message}`,
       }),
